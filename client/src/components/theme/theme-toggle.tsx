f1843f01f0ac5153760ec7c320ffc68ea35fd6b8
eno? <enoabasidream@gmail.com>
2025-06-09 20:36:18 +0100

    refac: Use `RemixIcons` icons for theme toggle

--- client/src/components/theme/theme-toggle.tsx
+++ client/src/components/theme/theme-toggle.tsx
@@ -1,8 +1,7 @@
-import { Moon, Sun } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
 import { useTheme } from "./theme-provider";
 import { useCallback } from "react";
+import { Icons } from "@/components/ui/icons";
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -19,8 +18,8 @@ export function ThemeToggle() {
       size="icon"
       onClick={toggle}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Icons.RemixIcons.SunFill className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Icons.RemixIcons.MoonClearFill className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
